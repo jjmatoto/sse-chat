d66110b94c7cd7709d50db18d6bbfb2ecee22b0a
Juan <jjmatotorres@gmail.com>
2016-11-20 16:55:43 +0100

    restyle

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,51 @@
-var src, out, form;
+var src, out, form, textarea, h = 1;
 $(document).ready(function(){
 	
 	src = new EventSource("api/chat/stream");
 	out = $("#out");
 	form = $("#chat_form");
-
-	
-	$("textarea[name=txt]").keypress(function(e) {
-		if(!e.shiftKey && e.which==13){
-		form.submit();
-		}
-	});
-	
-	function renderMsg(data){
-		var spanAut = "<strong>"+data.autor+":</strong>",
-			spanTxt = "<span>"+data.text+"</span>",
-			spanDate = "<span class=\"date\">"+new Date(data.date).toLocaleTimeString()+"</span>",
-			divMsg = "<div id=\"msg\">"+spanAut+spanTxt+spanDate+"</div>";
-			out.append(divMsg);
-	}
+	textarea = $("#input textarea[name='txt']");
 
 	src.onmessage = function(e) {
 		var data = JSON.parse(e.data);
 		renderMsg(data);
 	};
+
+	textarea.keypress(function(e) {
+		if(e.shiftKey && e.which==13){			
+			textarea.css('height', (h++)+'em');
+		}
+		else if(!e.shiftKey && e.which==13){
+			form.submit();
+			restTextArea();
+		}
+		// else if(e.which==27){ esc keycode
+		// 	restTextArea();
+		// }
+	});
+
+	function restTextArea(){
+		h= 1;
+		textarea.css('height', h+'em');
+		textarea.value = "";
+		textarea.focus();
+	}
+		
+	function renderMsg(data){
+		var spanAut = "<strong>"+data.autor+":</strong>",
+		spanTxt = "<span>"+data.text+"</span>",
+		spanDate = "<span class=\"date\">"+new Date(data.date).toLocaleTimeString()+"</span>",
+		divMsg = "<div id=\"msg\">"+spanAut+spanTxt+spanDate+"</div>";
+		out.append(divMsg);
+	}
+
+	form.on("submit", function(e){
+		e.preventDefault();
+		$.post(	"/api/chat", form.serialize())
+		.done(function( msg ) {
+			console.log( "Data Saved: " + msg );
+		});
+	});
 
 	$.ajax("api/chat").done(function(data){
 		var data = JSON.parse(data);
@@ -31,14 +53,4 @@ $(document).ready(function(){
 			renderMsg(item);
 		});
 	})
-
-	form.on("submit", function(e){
-		e.preventDefault();
-		$.post(	"/api/chat", form.serialize())
-		.done(function( msg ) {
-			console.log( "Data Saved: " + msg );
-			$("textarea[name=txt]")[0].value = "";
-			$("textarea[name=txt]")[0].focus();
-		});
-	});
 });
